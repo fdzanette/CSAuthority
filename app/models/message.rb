475ba6416f5b76da5c016fcd9f434fe63006d37b
fabricio zanette <fabzanette@gmail.com>
2019-03-08 13:47:29 +0000

    Final form styles and validations added.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,9 @@
 class Message < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :year, presence: true
+  validates :level, presence: true
+  validates :sport, presence: true
+
 end
